fa4826292e733b3af972fa8ee26a43f333111af2
AlenadroRentheria <manuel.rentheria@gmail.com>
2020-06-24 23:26:41 -0500

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatFormFieldModule,MatSelectModule,MatDatepickerModule,MatNativeDateModule,MatInputModule,MatTableModule,MatIconModule,MatPaginatorModule,MatSortModule,MatCheckboxModule,MatExpansionModule,MatButtonModule,MatDialogModule,MatMenuModule,MatSlideToggleModule,MatButtonToggleModule,MatAutocompleteModule,MatRadioModule,MatCardModule,MatTabsModule,MatTooltipModule,MatBadgeModule,MatBottomSheetModule,MatStepperModule,MatSliderModule,MatProgressBarModule} from '@angular/material';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatListModule} from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MatChipsModule,
+    MatListModule,
+    MatSortModule,
+    MatAutocompleteModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatBottomSheetModule,
+    MatTabsModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatSliderModule,
+    MatBadgeModule,
+    MatStepperModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
